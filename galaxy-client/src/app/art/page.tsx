1bd8d81590c42d41eb4f02c130f48d75fb4d23cd
scottXchoo <scottchoo@a41.io>
2023-03-03 13:27:09 +0900

    feat: setUp nextjs, typescript, graphql, apollo

---
+++ galaxy-client/src/app/art/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <p>Art page</p>
+    </main>
+  );
+}
---
+++ galaxy-client/src/app/pfps/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <p>PFPs page</p>
+    </main>
+  );
+}
