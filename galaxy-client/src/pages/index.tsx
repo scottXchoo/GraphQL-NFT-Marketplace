6cd5cf7b2ef236446a9622b2b503228b5779beca
scottXchoo <scottchoo@a41.io>
2023-03-06 20:09:49 +0900

    feat(client): footer

--- galaxy-client/src/pages/index.tsx
+++ galaxy-client/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "@/components/Header";
 import Dashboard from "@/components/Dashboard";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <main>
         <Header />
         <Dashboard />
+        <Footer />
       </main>
     </>
   );
