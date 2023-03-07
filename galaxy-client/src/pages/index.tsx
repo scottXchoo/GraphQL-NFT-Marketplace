d775663d696768bc405d1a9d94aa1c3176a14d63
scottXchoo <scottchoo@a41.io>
2023-03-07 19:53:13 +0900

    refactor(client): change title

--- galaxy-client/src/pages/index.tsx
+++ galaxy-client/src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Galaxy : NFT Marketplace in Cosmos</title>
+        <title>Galaxy : The Multi-chain NFT Marketplace</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
